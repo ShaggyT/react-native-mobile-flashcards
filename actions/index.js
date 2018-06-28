8c52b2e3285db885bc3a79f119be14abfa368839
Shaghayegh Taghipour <shaghayegh.taghipour@gmail.com>
2018-06-27 19:11:10 -0700

    Creating action creators

---
+++ actions/index.js
@@ -0,0 +1,27 @@
+import * as API from '../utils/api'
+import {
+  RECEIVE_DECKS,
+  ADD_DECK,
+} from './types'
+
+export function receiveDecks (decks) {
+  return {
+    type: RECEIVE_DECKS,
+    decks,
+  }
+}
+
+export function addDeck (title) {
+  return {
+    type: ADD_DECK,
+    title,
+  }
+}
+
+export function addCard (title, card) {
+  return {
+    type: ADD_CARD,
+    title,
+    card,
+  }
+}
--- utils/api.js
+++ utils/api.js
@@ -12,5 +12,4 @@ export const DECKS_STORAGE_KEY = 'MobileFlashcards:decks'
 // saveDeckTitle: take in a single title argument and add it to the decks.
 
 
-
 // addCardToDeck: take in two arguments, title and card, and will add the card to the list of questions for the deck with the associated title.
